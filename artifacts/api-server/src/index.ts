50bbc2460b175d4531470d6f62f6105e2a6141c7
waisshaikh <waisshaikh394@gmail.com>
2026-03-19 21:17:31 +0530

    update root route response message and clean up console log

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -2,6 +2,11 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
 
+
+app.get("/", (req, res) => {
+  res.send("API is running ");
+});
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
